618ec8fc5087b5ec4f5b0b5d068b92710a268146
aangelrs <angelrangel561@gmail.com>
2022-08-23 09:55:03 -0400

    mejorado index.js  V1.1

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import app from "./app.js";
 
 const main=() =>{
     let port = app.get("port");
-    app.listen(app.get("port"));
-    console.log('Serve on port ' + port );
+    app.listen(port);
+    console.log('🚀️ Serve on port ' + port + '🚀️');
 };
 
 main();
